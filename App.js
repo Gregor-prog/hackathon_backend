b95a3047a1362dd6e7cb0993949eaec08105012c
Greg-prog <olajideemmanuel250@gmail.com>
2025-02-23 15:23:18 +0000

    cors

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import express from "express"
+import cors from "cors"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
 let app = express()
@@ -20,6 +21,7 @@ import getData from "./controllers/getData.js"
 
 // using json formatted data
 app.use(express.json());
+app.use(cors({}))
 
 app.post("/postVitals", postData)
 app.get("/getVitals",getData)
